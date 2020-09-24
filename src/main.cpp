fc0b360038c84350724ba8e6c105eb674968eec2
Lennart Eichhorn <lennart@madmanfred.com>
2020-09-24 15:04:52 +0200

    Implemented main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <QCoreApplication>
+#include "server.h"
+#include "src/schedulerservice.h"
 
 int main(int argc, char* argv[]) {
   QCoreApplication a(argc, argv);
+  jsonrpc::Server<SchedulerService> server(9094);
+  server.setConstructorArguments();
+  server.startListening();
 
   return a.exec();
 }
