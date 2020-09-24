a660ca668efd32ad29f8c45f70b1970d0dc16735
Lennart Eichhorn <lennart@madmanfred.com>
2020-09-24 14:04:57 +0200

    Implemented SchedulerService

--- src/schedulerservice.cpp
+++ src/schedulerservice.cpp
@@ -1,20 +1,45 @@
 #include "schedulerservice.h"
 
-SchedulerService::SchedulerService(QObject* parent) : QObject(parent) {
-  // TODO implement
-}
+SchedulerService::SchedulerService(QObject* parent)
+    : QObject(parent),
+      scheduler(nullptr),
+      progress(0.0),
+      result(QJsonValue::Undefined) {}
 
-bool SchedulerService::startScheduling(QJsonValue plan) {
-  // TODO implement
-  return false;
+bool SchedulerService::startScheduling(QJsonObject plan) {
+  if (scheduler != nullptr) {
+    return false;
+  }
+
+  QSharedPointer<Plan> planPointer(new Plan());
+  planPointer->fromJsonObject(plan);
+
+  scheduler.reset(new LegacyScheduler(planPointer));
+  QObject::connect(
+      (LegacyScheduler*)scheduler.data(), &LegacyScheduler::updateProgress,
+      [this](double updatedProgress) { progress = updatedProgress; });
+  QObject::connect((LegacyScheduler*)scheduler.data(),
+                   &LegacyScheduler::failedScheduling,
+                   [this](QString errorMessage) {
+                     result = errorMessage;
+                     progress = 1.0;
+                   });
+  QObject::connect((LegacyScheduler*)scheduler.data(),
+                   &LegacyScheduler::finishedScheduling,
+                   [this](QSharedPointer<Plan> scheduledPlan) {
+                     result = scheduledPlan->toJsonObject();
+                     progress = 1.0;
+                   });
+
+  scheduler->startScheduling();
+
+  return true;
 }
 
 double SchedulerService::getProgress() {
-  // TODO implement
-  return 0.0;
+  return progress;
 }
 
 QJsonValue SchedulerService::getResult() {
-  // TODO implement
-  return QJsonValue(QJsonValue::Undefined);
+  return result;
 }
